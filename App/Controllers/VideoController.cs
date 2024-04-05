e858237f2898789bc6385381f6755a326196fba0
zumwani <andreas.ingeholm@gmail.com>
2024-04-05 19:08:06 +0200

    Added header. Setup controller for videos.

---
+++ App/Controllers/VideoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.Controllers;
+
+public class VideosController : Controller
+{
+    public IActionResult Search([FromQuery] string? q)
+    {
+        //TODO: Return using htmx
+        return StatusCode(200);
+    }
+
+    public IActionResult Home()
+    {
+        return StatusCode(200);
+    }
+
+    [Authorize]
+    public IActionResult Subscriptions()
+    {
+        return StatusCode(200);
+    }
+}
